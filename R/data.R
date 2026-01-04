cafae444fbb1afa89281d736efecf127189e290e
Alain_fixe <alain.luong@student-cs.fr>
2026-01-04 16:20:02 +0100

    add documentation for data ecsi and russett

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' European Customer Satisfaction Index Data
+#'
+#' @description
+#' Dataset containing customer satisfaction measurements for European companies.
+#' This dataset is commonly used for structural equation modeling examples
+#' involving customer satisfaction indices.
+#'
+#' @format A table object
+#'
+#' @examples
+#' \dontrun{
+#' data(ECSI)
+#' head(ECSI)
+#' }
+"ECSI"
+
+#' Russett Dataset
+#'
+#' @description
+#' Dataset containing socio-economic and political variables for various countries.
+#' Often used for demonstrating path analysis and structural equation modeling techniques.
+#'
+#' @format A table object
+#'
+#' @examples
+#' \dontrun{
+#' data(Russett)
+#' head(Russett)
+#' }
+"Russett"
