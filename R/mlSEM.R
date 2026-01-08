108091137e9017088ba72c51493bcc497c1b0f13
Alain_pc_pro <alain.luong@centralesupelec.fr>
2026-01-08 16:58:13 +0100

    add tolerance as input

--- R/mlSEM.R
+++ R/mlSEM.R
@@ -29,7 +29,7 @@
 #' @importFrom Rsolnp solnp
 #'
 #' @export
-mlSEM <- function (init, S, model){
+mlSEM <- function (init, S, model, tol = 1e-08){
 
   mode <- model$mode
   # number of formative blocks
@@ -40,12 +40,12 @@ mlSEM <- function (init, S, model){
   result <- solnp(pars = init,
                   fun=F1, eqfun=heq1,
                   eqB = rep(0,r), S = S, model = model,
-                  control = list(trace = 0, tol = 1e-8))
+                  control = list(trace = 0, tol = tol))
   }
   else{
     result <- solnp(pars = init,
                     fun=F1, S = S, model = model,
-                    control = list(trace = 0, tol = 1e-8))
+                    control = list(trace = 0, tol = tol))
 
   }
 
