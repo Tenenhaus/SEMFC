62871b63c6fdd915632149603183467ac2a7bc7b
Alain_fixe <alain.luong@student-cs.fr>
2026-08-13 23:43:02 +0200

    add result printing scripts to run_experiment_one_step

--- inst/os_experiment/run_experiment_one_step.R
+++ inst/os_experiment/run_experiment_one_step.R
@@ -1,4 +1,9 @@
 
-# source("inst/os_experiment/experiment_one_step.R")
+source("inst/os_experiment/experiment_one_step.R")
 source("inst/os_experiment/experiment_one_step_blocks.R")
 source('inst/os_experiment/experiment_one_step_indicators.R')
+
+
+source("inst/os_experiment/print_result_n.R")
+source("inst/os_experiment/print_result_q.R")
+source("inst/os_experiment/print_result_J.R")
